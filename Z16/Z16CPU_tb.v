be738211e5a3762ca9204c5e6c1b67ce5a932a28
Jun-robot <saijun721.1@gmail.com>
2025-01-26 21:29:20 +0900

    add MMIO_button

--- Z16/Z16CPU_tb.v
+++ Z16/Z16CPU_tb.v
@@ -2,6 +2,7 @@ module Z16CPU_tb;
 
   reg i_clk = 1'b0;
   reg i_rst = 1'b0;
+  reg i_button = 1'b0;
   wire [5:0] o_led;
 
   always #1 begin
@@ -16,6 +17,7 @@ module Z16CPU_tb;
   Z16CPU CPU(
     .i_clk  (i_clk  ),
     .i_rst  (i_rst  ),
+    .i_button(i_button),
     .o_led  (o_led  )
   );
 
@@ -25,6 +27,12 @@ module Z16CPU_tb;
     #2
     // リセットを落とし、実行開始
     i_rst   = 1'b0;
+    #100
+    // 100クロック後にボタンを押す
+    i_button = 1'b1;
+    #20
+    // 20クロック後にボタンを離す
+    i_button = 1'b0;
     #100
     $finish;
   end
